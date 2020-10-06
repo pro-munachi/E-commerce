b54d1c8f3cfb0f675f96774e5ac074683f5b0e45
madu munachi <madustanley1@gmail.com>
2020-10-06 06:54:51 +0100

    'product-rating'

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Container } from 'react-bootstrap'
+import HomeScreen from './screens/HomeScreen'
 
-function App() {
+const App = () => {
   return (
-    <div className='App'>
-      <h1>Muna</h1>
-    </div>
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
+    </>
   )
 }
 
