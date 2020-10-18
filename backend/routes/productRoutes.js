46ae06e286f57c1823ba624758620420eba176eb
madu munachi <madustanley1@gmail.com>
2020-10-18 19:22:09 +0100

    'clean-up-by-controllers'

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,35 +1,12 @@
 const express = require('express')
-const asyncHandler = require('express-async-handler')
 const router = express.Router()
-const Product = require('../models/productModel')
+const {
+  getProducts,
+  getProductById
+} = require('../controllers/productController')
 
-//@desc    Fetch all products
-//@route   GET /api/product
-//@access  Public
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({})
+router.route('/').get(getProducts)
 
-    res.json(products)
-  })
-)
-
-//@desc    Fetch single product
-//@route   GET /api/product/:id
-//@access  Public
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id)
-
-    if (product) {
-      res.json(product)
-    } else {
-      res.status(404)
-      throw new Error('Product not found')
-    }
-  })
-)
+router.route('/:id').get(getProductById)
 
 module.exports = router
