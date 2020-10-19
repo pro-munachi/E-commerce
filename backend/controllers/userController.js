78ae656c26d75720177b15c1b9bb01dc48b140da
madu munachi <madustanley1@gmail.com>
2020-10-19 10:04:03 +0100

    'generate-token'

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 const asyncHandler = require('express-async-handler')
+const generateToken = require('../utils/generateToken')
 const User = require('../models/userModel')
 
 //@desc    Auth user & get token
@@ -16,7 +17,7 @@ const authUser = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: null
+      token: generateToken(user._id)
     })
   } else {
     res.status(401)
