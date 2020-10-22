5c2530af0d2cd0150cf90ff64f6a37c6f1b3da78
madu munachi <madustanley1@gmail.com>
2020-10-22 15:38:17 +0100

    'update-profile-endpoint'

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,12 +3,16 @@ const router = express.Router()
 const {
   authUser,
   getUserProfile,
-  registerUser
+  registerUser,
+  updateUserProfile
 } = require('../controllers/userController')
 const { protect } = require('../middleware/authMiddleware')
 
 router.route('/').post(registerUser)
 router.post('/login', authUser)
-router.route('/profile').get(protect, getUserProfile)
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile)
 
 module.exports = router
