45f317f6ddf90d75c11bbe7da9b6bc78a360f12c
madu munachi <madustanley1@gmail.com>
2020-10-24 20:59:06 +0100

    'profile-screen-and-get-user-details'

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -6,7 +6,10 @@ import {
   USER_LOGOUT,
   USER_REGISTER_REQUEST,
   USER_REGISTER_FAIL,
-  USER_REGISTER_SUCCESS
+  USER_REGISTER_SUCCESS,
+  USER_DETAILS_REQUEST,
+  USER_DETAILS_SUCCESS,
+  USER_DETAILS_FAIL
 } from '../constants/userConstants'
 
 export const login = (email, password) => async dispatch => {
@@ -16,7 +19,9 @@ export const login = (email, password) => async dispatch => {
     })
 
     const config = {
-      'Content-Type': 'application/json'
+      headers: {
+        'Content-Type': 'application/json'
+      }
     }
 
     const { data } = await axios.post(
@@ -54,7 +59,9 @@ export const register = (name, email, password) => async dispatch => {
     })
 
     const config = {
-      'Content-Type': 'application/json'
+      headers: {
+        'Content-Type': 'application/json'
+      }
     }
 
     const { data } = await axios.post(
@@ -77,6 +84,41 @@ export const register = (name, email, password) => async dispatch => {
   } catch (error) {
     dispatch({
       type: USER_REGISTER_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message
+    })
+  }
+}
+
+export const getUserDetails = id => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: USER_DETAILS_REQUEST
+    })
+
+    const {
+      userLogin: { userInfo }
+    } = getState()
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${userInfo.token}`
+      }
+    }
+
+    const { data } = await axios.get(`/api/users/${id}`, config)
+    console.log(data)
+
+    dispatch({
+      type: USER_DETAILS_SUCCESS,
+      payload: data
+    })
+  } catch (error) {
+    dispatch({
+      type: USER_DETAILS_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
