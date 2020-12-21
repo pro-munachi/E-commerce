41aec1b7c191f7de1d771c98828eb399fb46a5f4
madu munachi <madustanley1@gmail.com>
2020-12-21 08:09:08 +0100

    'create-and-update-product-endpoints'

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,14 +3,20 @@ const router = express.Router()
 const {
   getProducts,
   getProductById,
-  deleteProduct
+  deleteProduct,
+  createProduct,
+  updateProduct
 } = require('../controllers/productController')
 const { protect, admin } = require('../middleware/authMiddleware')
 
-router.route('/').get(getProducts)
+router
+  .route('/')
+  .get(getProducts)
+  .post(protect, admin, createProduct)
 router
   .route('/:id')
   .get(getProductById)
   .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct)
 
 module.exports = router
