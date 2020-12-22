c553b971625611716dc10917e30620b3ada34d39
madu munachi <madustanley1@gmail.com>
2020-12-22 21:11:34 +0100

    'deliver-orders'

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -4,6 +4,7 @@ const {
   addOrderItems,
   getOrderById,
   updateOrderToPaid,
+  updateOrderToDelivered,
   getMyOrders,
   getOrders
 } = require('../controllers/orderController')
@@ -16,5 +17,6 @@ router
 router.route('/myorders').get(protect, getMyOrders)
 router.route('/:id').get(protect, getOrderById)
 router.route('/:id/pay').put(protect, updateOrderToPaid)
+router.route('/:id/deliver').put(protect, admin, updateOrderToDelivered)
 
 module.exports = router
