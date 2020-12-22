f7a60e185075e43754eeac3c6bc449221e29b83f
madu munachi <madustanley1@gmail.com>
2020-12-22 08:49:54 +0100

    'front-end-image-upload'

--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Form, Button } from 'react-bootstrap'
@@ -18,6 +19,7 @@ const ProductEditScreen = ({ match, history }) => {
   const [category, setCategory] = useState('')
   const [countInStock, setCountInStock] = useState(0)
   const [description, setDescription] = useState('')
+  const [uploading, setUploading] = useState(false)
 
   const dispatch = useDispatch()
 
@@ -48,7 +50,30 @@ const ProductEditScreen = ({ match, history }) => {
         setDescription(product.description)
       }
     }
-  }, [product, dispatch, productId, history, successUpdate])
+  }, [dispatch, history, productId, product, successUpdate])
+
+  const uploadFileHandler = async e => {
+    const file = e.target.files[0]
+    const formData = new FormData()
+    formData.append('image', file)
+    setUploading(true)
+
+    try {
+      const config = {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      }
+
+      const { data } = await axios.post('/api/upload', formData, config)
+
+      setImage(data)
+      setUploading(false)
+    } catch (error) {
+      console.error(error)
+      setUploading(false)
+    }
+  }
 
   const submitHandler = e => {
     e.preventDefault()
@@ -92,7 +117,7 @@ const ProductEditScreen = ({ match, history }) => {
             </Form.Group>
 
             <Form.Group controlId='price'>
-              <Form.Label> Price</Form.Label>
+              <Form.Label>Price</Form.Label>
               <Form.Control
                 type='number'
                 placeholder='Enter price'
@@ -109,20 +134,27 @@ const ProductEditScreen = ({ match, history }) => {
                 value={image}
                 onChange={e => setImage(e.target.value)}
               ></Form.Control>
+              <Form.File
+                id='image-file'
+                label='Choose File'
+                custom
+                onChange={uploadFileHandler}
+              ></Form.File>
+              {uploading && <Loader />}
             </Form.Group>
 
             <Form.Group controlId='brand'>
               <Form.Label>Brand</Form.Label>
               <Form.Control
                 type='text'
-                placeholder='Enter brand '
+                placeholder='Enter brand'
                 value={brand}
                 onChange={e => setBrand(e.target.value)}
               ></Form.Control>
             </Form.Group>
 
             <Form.Group controlId='countInStock'>
-              <Form.Label> Count In Stock</Form.Label>
+              <Form.Label>Count In Stock</Form.Label>
               <Form.Control
                 type='number'
                 placeholder='Enter countInStock'
@@ -135,7 +167,7 @@ const ProductEditScreen = ({ match, history }) => {
               <Form.Label>Category</Form.Label>
               <Form.Control
                 type='text'
-                placeholder='Enter category '
+                placeholder='Enter category'
                 value={category}
                 onChange={e => setCategory(e.target.value)}
               ></Form.Control>
@@ -145,7 +177,7 @@ const ProductEditScreen = ({ match, history }) => {
               <Form.Label>Description</Form.Label>
               <Form.Control
                 type='text'
-                placeholder='Enter description '
+                placeholder='Enter description'
                 value={description}
                 onChange={e => setDescription(e.target.value)}
               ></Form.Control>
