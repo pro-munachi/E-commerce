00c4168e8e9f47a6bb7154f84a6bd5872e31e259
madu munachi <madustanley1@gmail.com>
2020-12-22 09:47:34 +0100

    'admin-order-list'

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -4,11 +4,15 @@ const {
   addOrderItems,
   getOrderById,
   updateOrderToPaid,
-  getMyOrders
+  getMyOrders,
+  getOrders
 } = require('../controllers/orderController')
-const { protect } = require('../middleware/authMiddleware')
+const { protect, admin } = require('../middleware/authMiddleware')
 
-router.route('/').post(protect, addOrderItems)
+router
+  .route('/')
+  .post(protect, addOrderItems)
+  .get(protect, admin, getOrders)
 router.route('/myorders').get(protect, getMyOrders)
 router.route('/:id').get(protect, getOrderById)
 router.route('/:id/pay').put(protect, updateOrderToPaid)
