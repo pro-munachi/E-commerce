add3e76e39ec5b8f7d376ef132edcc67f5bc10c2
madu munachi <madustanley1@gmail.com>
2020-12-24 21:22:35 +0100

    'product-pagination'

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -6,6 +6,8 @@ const Product = require('../models/productModel')
 //@access  Public
 
 const getProducts = asyncHandler(async (req, res) => {
+  const pageSize = 2
+  const page = Number(req.query.pageNumber) || 1
   const keyword = req.query.keyword
     ? {
         name: {
@@ -15,9 +17,12 @@ const getProducts = asyncHandler(async (req, res) => {
       }
     : {}
 
+  const count = await Product.countDocuments({ ...keyword })
   const products = await Product.find({ ...keyword })
+    .limit(pageSize)
+    .skip(pageSize * (page - 1))
 
-  res.json(products)
+  res.json({ products, page, pages: Math.ceil(count / pageSize) })
 })
 
 //@desc    Fetch single product
