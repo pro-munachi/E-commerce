c0e8351da1d3c7bb9d7843871f906e142fe008a6
madu munachi <madustanley1@gmail.com>
2021-02-13 21:24:20 +0100

    page size

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -6,7 +6,7 @@ const Product = require('../models/productModel')
 //@access  Public
 
 const getProducts = asyncHandler(async (req, res) => {
-  const pageSize = 4
+  const pageSize = 5
   const page = Number(req.query.pageNumber) || 1
   const keyword = req.query.keyword
     ? {
