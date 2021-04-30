fd642c7e2ac5587da13f9f4f23725fdc97eb84af
pro-munachi <madustanley1@gmail.com>
2021-04-30 11:06:01 +0100

    prepare for deployment

--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -97,7 +97,7 @@ const ProductEditScreen = ({ match, history }) => {
         Go Back
       </Link>
       <FormContainer>
-        <h1>Edit Product</h1>
+        <h1 style={{ color: '#0f3959' }}>Edit Product</h1>
         {loadingUpdate && <Loader />}
         {errorUpdate && <Message variant='danger'>{errorUpdate}</Message>}
         {loading ? (
